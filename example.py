e5dbc3afa58aedd65e115a643789d0c2f9a658c1
Kye <kye@apacmediasolutions.com>
2023-09-07 12:54:59 -0400

    DOCUMENTATION for `Poly`

--- example.py
+++ example.py
@@ -1,57 +1,6 @@
-from typing import Any, TypeVar, Generic
-import pickle
-
-T = TypeVar('T')
-
-class Poly(Generic[T]):
-    def __init__(self, data: Any):
-        self.data = data
-        self.type_mapping = {}
-        self.alias_mapping = {}
-
-    def determine(self):
-        data = type(self.data)
-        if data not in self.type_mapping:
-            self.type_mapping[data] = data
-        return self.type_mapping[data]
-
-    def select(self):
-        return self.determine()
-
-    def shift(self, target):
-        try:
-            return target(self.data)
-        except ValueError:
-            raise TypeError(f"Cannot shift {self.data} to {target}")
-
-    def validate(self, target):
-        if not isinstance(self.data, target):
-            raise TypeError(f"{self.data} is not of type {target}")
-        return True
-
-    def add_alias(self, alias, target):
-        self.alias_mapping[alias] = target
-
-    def annotate(self, annotation):
-        self.data: annotation
-
-    def extend(self, extension):
-        class ExtendedType(type(self.data), extension):
-            pass
-        self.data = ExtendedType(self.data)
-
-    def serialize(self):
-        return pickle.dumps(self.data)
-
-    def deserialize(self, serialized_data):
-        self.data = pickle.loads(serialized_data)
-        return self.data
+# !pip install shapeless
+from shapeless.main import Poly
     
-    def __instancecheck__(self, instance):
-        return isinstance(instance, self.select())
-    
-
-
 def my_func(a: Poly):
     print(type(a))
 
--- shapeless/main.py
+++ shapeless/main.py
@@ -6,62 +6,132 @@ import pickle
 T = TypeVar('T')
 
 class Poly(Generic[T]):
-    def __init__(
-            self, 
-            data: Any, 
-            verbose: bool = False
-        ):
+    """
+    The Poly class is a utility class that provides dynamic type handling.
+    It allows you to determine, select, shift, validate, alias, annotate, extend, serialize, and deserialize types.
+    It also provides thread safety and optional logging.
+    """
+
+    def __init__(self, data: Any, verbose: bool = False):
+        """
+        Initialize a new Poly object.
+
+        :param data: The data whose type is to be handled.
+        :param verbose: If True, log all operations. Default is False.
+        """
         self.data = data
+        self.verbose = verbose
         self.type_mapping = {}
         self.alias_mapping = {}
         self.lock = threading.Lock()
         if self.verbose:
             logging.info(f"Created a new Poly object with data: {self.data}")
 
-    
     def determine(self):
+        """
+        Determine the type of the data.
+
+        :return: The type of the data.
+        """
         with self.lock:
-            data = type(self.data)
-            if data not in self.type_mapping:
-                self.type_mapping[data] = data
+            data_type = type(self.data)
+            if data_type not in self.type_mapping:
+                self.type_mapping[data_type] = data_type
             if self.verbose:
-                logging.info(f"Determines type of data: {data}")
-            return self.type_mapping[data]
-    
+                logging.info(f"Determined type of data: {data_type}")
+            return self.type_mapping[data_type]
+
     def select(self, target):
-        selected =  self.determine()
+        """
+        Select the type of the data.
+
+        :param target: The target type.
+        :return: The selected type.
+        """
+        selected_type = self.determine()
         if self.verbose:
-            logging.info(f"Selected type: {selected}")
-        return selected
-    
+            logging.info(f"Selected type: {selected_type}")
+        return selected_type
+
     def shift(self, target):
+        """
+        Attempt to shift the data to the target type.
+
+        :param target: The target type.
+        :return: The data after shifting to the target type.
+        :raises TypeError: If the data cannot be shifted to the target type.
+        """
         try:
             return target(self.data)
         except ValueError:
+            if self.verbose:
+                logging.error(f"Failed to shape shift {self.data} to {target}")
             raise TypeError(f"Cannot shape shift {self.data} to {target}")
-    
+
     def validate(self, target):
+        """
+        Validate that the data is of the target type.
+
+        :param target: The target type.
+        :return: True if the data is of the target type, False otherwise.
+        :raises TypeError: If the data is not of the target type.
+        """
         if not isinstance(self.data, target):
+            if self.verbose:
+                logging.error(f"{self.data} is not of type {target}")
             raise TypeError(f"{self.data} is not of type {target}")
         return True
-    
+
     def add_alias(self, alias, target):
+        """
+        Add an alias for a type.
+
+        :param alias: The alias.
+        :param target: The target type.
+        """
         self.alias_mapping[alias] = target
 
     def annotate(self, annotation):
+        """
+        Annotate the data with a type.
+
+        :param annotation: The type annotation.
+        """
         self.data: annotation
-    
+
     def extend(self, extension):
+        """
+        Extend the type of the data with a new type.
+
+        :param extension: The new type.
+        """
         class ExtendedType(type(self.data), extension):
             pass
         self.data = ExtendedType(self.data)
-    
+
     def serialize(self):
+        """
+        Serialize the data.
+
+        :return: The serialized data.
+        """
         return pickle.dumps(self.data)
-    
+
     def deserialize(self, serialized_data):
+        """
+        Deserialize the data.
+
+        :param serialized_data: The serialized data.
+        :return: The deserialized data.
+        """
         self.data = pickle.loads(serialized_data)
         return self.data
-    
+
     def __instancecheck__(self, instance):
+        """
+        Check if an instance is of the selected type.
+
+        :param instance: The instance to check.
+        :return: True if the instance is of the selected type, False otherwise.
+        """
         return isinstance(instance, self.select())
