5b340cf48c03ae9a773d1d5d421054d6614b6439
Kye <kye@apacmediasolutions.com>
2023-09-18 16:30:23 -0400

    clean up

---
+++ shapeless/meta_learner.py
@@ -0,0 +1,11 @@
+"""
+This page is reserved for an algorithm that will create an all-new type at runtime,
+with megabyte to not be restricted at all.
+
+data => meta learner model that learns type => generates new type and holds it in cache
+
+
+# Ideas
+- Meta learners like Koi, with evolutionary algos
+- Terrabyte but really small, nn that learns type and creates byte stream of new type
+"""
